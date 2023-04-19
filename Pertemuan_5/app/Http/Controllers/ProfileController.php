977b84b9a4c59a6f475cf9206a4c47db4755ec20
Katkatarr <katarinalaurentia@gmail.com>
2023-04-19 10:45:56 +0700

    Pertemuan 5

---
+++ Pertemuan_5/app/Http/Controllers/ProfileController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\biodata;
+use Illuminate\Http\Request;
+
+
+class ProfileController extends Controller
+{
+    public function profile(){
+        $biodata = biodata::all();
+       // dd($student);
+       return view('profile', compact('biodata'));
+ }
+}
+
